867faa7ea46b7a41c0bb76e27595a0fd70de9200
mecpc2 <danielbrumpy@gmail.com>
2026-08-17 22:27:09 -0300

    Primeiro commit

---
+++ Extraçao_do_banco.py
@@ -0,0 +1,18 @@
+import pandas as pd
+from sqlalchemy import create_engine
+
+df = pd.read_csv("/home/mec/Documentos/vscode/python/Projeto-integraçao/samp-2026.csv",encoding="latin1", sep = ";")
+
+
+engine = create_engine(
+    "mysql+mysqlconnector://root:Linux2314@localhost/energia"
+)
+
+df.to_sql(
+    "samp_2026",
+    con=engine,
+    if_exists="replace",
+    index=False
+)
+
+print("Dados enviados para o MySQL!")
